cb8e4363105cb4895130f0592a56fc26743cbb60
milinddevops <ashwinichavan.22@gmail.com>
2022-03-20 01:43:14 +0530

    retrun removed

--- src/com.mrc/stages/CheckoutStage.groovy
+++ src/com.mrc/stages/CheckoutStage.groovy
@@ -3,8 +3,7 @@ package com.mrc.stages
 import com.mrc.logic.Checkout
 
 void action() {
-    return stage('Checout') {
-        
-        return new Checout.action()
+    stage('Checout') {        
+         new Checout.action()
     }
 }
