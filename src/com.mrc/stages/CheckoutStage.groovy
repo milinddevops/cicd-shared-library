e881c3260f65c907fea14f62208ad9419bfe136a
milinddevops <ashwinichavan.22@gmail.com>
2022-03-20 01:56:24 +0530

    stage added

--- src/com.mrc/stages/CheckoutStage.groovy
+++ src/com.mrc/stages/CheckoutStage.groovy
@@ -2,12 +2,8 @@ package com.mrc.stages
 
 import com.mrc.logic.Checkout
 
-def checkOutFrom() {
-    checkout(scm: jenkins.scm)
+void action() {
+    stage('Checout') {        
+         checkout(scm: jenkins.scm)
+    }
 }
-
-return this
-
-/*void action() {     
-    new Checout.action()
-}*/
