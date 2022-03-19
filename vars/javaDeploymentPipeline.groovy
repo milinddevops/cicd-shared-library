c4dc4b12d454bf4d82304166ed3087360bb3c846
milinddevops <ashwinichavan.22@gmail.com>
2022-03-20 02:10:55 +0530

    com removed

--- vars/javaDeploymentPipeline.groovy
+++ vars/javaDeploymentPipeline.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
-import com.mrc.stages.CheckoutStage
+import mrc.stages.CheckoutStage
 /*import com.mrc.stages.InitStage
 import com.mrc.stages.BuildStage
 import com.mrc.stages.DeployStage*/
@@ -8,7 +8,7 @@ import com.mrc.stages.DeployStage*/
 def call() {
     utility.runPipeline('deployPOd.yaml') {
         try {
-            new CheckoutStage().action()
+            new CheckoutStage().invoke()
         } catch(error) {
         }
     }
