c9f148692b6b63c75681c788efd06052da46ea31
milinddevops <ashwinichavan.22@gmail.com>
2022-03-20 20:27:19 +0530

    cc

--- src/mrc/common/pipelineState/pipelineData.groovy
+++ src/mrc/common/pipelineState/pipelineData.groovy
@@ -10,7 +10,7 @@ class pipelineData {
     }
 
     static void setJobParams(def jenkins, List applicationNames) {
-        JenkinsPropertiesParameters jpp = new jenkinspropertiesparameters()
+        jenkinspropertiesparameters jpp = new jenkinspropertiesparameters()
 
         jenkins.properties([
             jenkins.parameters([
