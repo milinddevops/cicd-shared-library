0660abdfce40552170368b5a85d09c4152e24808
milinddevops <ashwinichavan.22@gmail.com>
2022-03-20 22:52:37 +0530

    cc

--- src/mrc/common/pipelineState/pipelineData.groovy
+++ src/mrc/common/pipelineState/pipelineData.groovy
@@ -10,12 +10,12 @@ class pipelineData {
     }
 
     static void setJobParams(def jenkins, List applicationNames) {
-        jenkinspropertiesparameters jpp = new jenkinspropertiesparameters()
+        //jenkinspropertiesparameters jpp = new jenkinspropertiesparameters()
         
-        /*properties([
+        properties([
             parameters([
                 choice(name: 'size', choices: ['S', 'M', 'L'],  description: 'Size choice (S/M/L)')
-        ])]) */
+        ])])
         /*properties([
             parameters([
                 choice(name: 'CHOICES', choices: applicationNames, description: 'This is a list of Application to be deployed'
