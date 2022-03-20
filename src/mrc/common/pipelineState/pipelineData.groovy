8bcdf32b32c38f21787bda5f00e58e4f15cf5702
milinddevops <ashwinichavan.22@gmail.com>
2022-03-20 19:57:08 +0530

    app name hard coded

--- src/mrc/common/pipelineState/pipelineData.groovy
+++ src/mrc/common/pipelineState/pipelineData.groovy
@@ -14,11 +14,8 @@ class pipelineData {
 
         jenkins.properties([
             jenkins.parameters([
-                choice(name: 'CHOICES', 
-                choices: applicationNames, 
-                description: 'This is a list of Application to be deployed')
-            ])
-        ])
+                choice(name: 'CHOICES', choices: ['pet-clinic', 'clinic'], description: 'This is a list of Application to be deployed')
+        ])])
     }
 
 }
