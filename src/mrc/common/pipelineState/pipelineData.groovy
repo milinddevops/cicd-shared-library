0ffd020bfeabf25ade9cab07475ca1f243091510
milinddevops <ashwinichavan.22@gmail.com>
2022-03-20 20:20:44 +0530

    code fixed

--- src/mrc/common/pipelineState/pipelineData.groovy
+++ src/mrc/common/pipelineState/pipelineData.groovy
@@ -10,11 +10,11 @@ class pipelineData {
     }
 
     static void setJobParams(def jenkins, List applicationNames) {
-        //jenkinspropertiesparameters jpp = new jenkinspropertiesparameters()
+        jenkinspropertiesparameters jpp = new jenkinspropertiesparameters()
 
         jenkins.properties([
             jenkins.parameters([
-                choice(name: 'CHOICES', choices: applicationNames, description: 'This is a list of Application to be deployed')
+                jpp.choice(applicationNames)
         ])])
     }
 
