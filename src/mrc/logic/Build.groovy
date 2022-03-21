aef375b98ff2620482a6592080443b9d97112e66
milinddevops <ashwinichavan.22@gmail.com>
2022-03-21 20:57:44 +0530

    cd dir cmd added

--- src/mrc/logic/Build.groovy
+++ src/mrc/logic/Build.groovy
@@ -4,6 +4,7 @@ import mrc.common.pipelineState.pipelineData
 
 def action(pipelineinfo) {
     sh 'ls -ltr ./pet-clinic'
-    sh 'mvn clean package ./pet-clinic'
+    sh 'cd pet-clinic'
+    sh 'mvn clean package'
     sh 'ls -ltr ./pet-clinic'
 }
