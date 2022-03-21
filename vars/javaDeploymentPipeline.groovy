fb89939772f1e8e399c073512d5a65afcc993da3
milinddevops <ashwinichavan.22@gmail.com>
2022-03-21 17:53:36 +0530

    cc

--- vars/javaDeploymentPipeline.groovy
+++ vars/javaDeploymentPipeline.groovy
@@ -22,7 +22,7 @@ def call(List applications) {
         try {
             new CheckoutStage().action()
             
-            new InitStage().action()
+            def obj = new InitStage().action()
 
         } catch(error) {
         }
