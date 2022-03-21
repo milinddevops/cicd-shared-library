56cea7c0ca888e87159e773eb00beb0847f1e1b3
milinddevops <ashwinichavan.22@gmail.com>
2022-03-22 00:24:33 +0530

    CC

--- src/mrc/logic/Init.groovy
+++ src/mrc/logic/Init.groovy
@@ -6,6 +6,6 @@ import mrc.common.pipelineState.pipelineData
 def action(pipelineinfo) {    
     echo "Initalizing for: " + pipelineinfo.applicationName
     pipelineinfo.jenkins.currentBuild.displayName = "${pipelineinfo.applicationName}-BUILD-${env.BUILD_NUMBER}"
-    pipelineinfo.imageTag = "${pipelineinfo.applicationName}-BUILD-${env.BUILD_NUMBER}"
+    //pipelineinfo.imageTag = "${pipelineinfo.applicationName}-BUILD-${env.BUILD_NUMBER}"
     return pipelineinfo
 }
