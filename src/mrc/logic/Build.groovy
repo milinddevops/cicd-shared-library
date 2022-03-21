ee333fb2f372d7f9c6427377f779a4058414cc50
milinddevops <ashwinichavan.22@gmail.com>
2022-03-21 20:55:30 +0530

    dir added

--- src/mrc/logic/Build.groovy
+++ src/mrc/logic/Build.groovy
@@ -3,7 +3,7 @@ package mrc.logic
 import mrc.common.pipelineState.pipelineData
 
 def action(pipelineinfo) {
-    sh 'ls -ltr'
+    sh 'ls -ltr ./pet-clinic'
     sh 'mvn clean package ./pet-clinic'
     sh 'ls -ltr ./pet-clinic'
 }
