f1fd691c30e11247fbee0cccb8eb855d02e0e04b
milinddevops <ashwinichavan.22@gmail.com>
2022-03-21 15:54:44 +0530

    CC

--- src/mrc/logic/Init.groovy
+++ src/mrc/logic/Init.groovy
@@ -1,5 +1,6 @@
 package mrc.logic
 
 void action() {
-    currentBuild.name = "Test Build" env.BUILD_NUMBER
+    currentBuild.name = "Test Build" + env.BUILD_NUMBER
+    println("Came here in Init")
 }
