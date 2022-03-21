f2d4a361026f0a847ad658b90578adec3d7ddb3c
milinddevops <ashwinichavan.22@gmail.com>
2022-03-22 00:37:21 +0530

    docker user pass updated

--- src/mrc/logic/Docker.groovy
+++ src/mrc/logic/Docker.groovy
@@ -6,7 +6,7 @@ def action(pipelineinfo) {
     sh "cd ${pipelineinfo.applicationName}; docker build -t ${pipelineinfo.imageTag} ."
 
     withCredentials([usernamePassword(credentialsId: 'milinddocker', passwordVariable: 'milinddockerPassword', usernameVariable: 'milinddockerUser')]) {
-        sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPassword}"
+        sh "docker login -u ${env.milinddockerUser} -p ${env.milinddockerPassword}"
         sh "docker push milinddocker/cicd::${pipelineinfo.imageTag}"
     }
 
