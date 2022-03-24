6fe6aba6c796e0df3b4ba9c4a3de56b568d6daa4
milinddevops <ashwinichavan.22@gmail.com>
2022-03-24 23:26:52 +0530

    docker container added

--- src/mrc/logic/Docker.groovy
+++ src/mrc/logic/Docker.groovy
@@ -4,16 +4,21 @@ import mrc.common.pipelineState.pipelineData
 //import mrc.common.pipelineUtility
 
 def action(pipelineinfo) {
-    sh "cd ${pipelineinfo.applicationName}; docker build -t ${pipelineinfo.imageTag} ."
 
-    /*pipelineUtility.buildDockerImg(pipelineinfo) {
+    container('docker') {
+        sh "docker login -u ${env.milinddockerUser} -p ${env.milinddockerPassword}"
+    }
 
-    }*/
+    /*sh "cd ${pipelineinfo.applicationName}; docker build -t ${pipelineinfo.imageTag} ."
+
+    *//*pipelineUtility.buildDockerImg(pipelineinfo) {
+
+    }*//*
 
     withCredentials([usernamePassword(credentialsId: 'milinddocker', passwordVariable: 'milinddockerPassword', usernameVariable: 'milinddockerUser')]) {
         sh "docker login -u ${env.milinddockerUser} -p ${env.milinddockerPassword}"
         sh "docker push milinddocker/cicd::${pipelineinfo.imageTag}"
-    }
+    }*/
 
     return pipelineinfo
 }
