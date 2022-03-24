8d253d42ac9e3dd7c05a6a7e360be940be6860d5
milinddevops <ashwinichavan.22@gmail.com>
2022-03-25 00:48:16 +0530

    container added for build logic

--- src/mrc/logic/Build.groovy
+++ src/mrc/logic/Build.groovy
@@ -2,8 +2,10 @@ package mrc.logic
 
 import mrc.common.pipelineState.pipelineData
 
-def action(pipelineinfo) {    
-    sh 'cd pet-clinic; ls -ltr; mvn clean package;ls -ltr' 
+def action(pipelineinfo) {
+    container('maven') {
+        sh 'cd pet-clinic; ls -ltr; mvn clean package;ls -ltr'
+    }
 
     return pipelineinfo   
 }
