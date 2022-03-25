d1cf1e4dfa41e2821b1f7cc5752f214681e07c71
milinddevops <ashwinichavan.22@gmail.com>
2022-03-25 23:50:53 +0530

    pod yml changed

--- src/mrc/logic/Build.groovy
+++ src/mrc/logic/Build.groovy
@@ -3,7 +3,7 @@ package mrc.logic
 import mrc.common.pipelineState.pipelineData
 
 def action(pipelineinfo) {
-    container('maven') {
+    container('jnlp') {
         sh 'hostname'
         sh 'cd pet-clinic; ls -ltr; mvn clean package;ls -ltr'
     }
