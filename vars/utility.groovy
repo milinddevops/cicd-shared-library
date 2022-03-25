96593f7b78e13e0efba3f8e2add608fce3677f28
milinddevops <ashwinichavan.22@gmail.com>
2022-03-25 23:06:02 +0530

    pod yml changed

--- vars/utility.groovy
+++ vars/utility.groovy
@@ -1,7 +1,10 @@
 void runPipeline(String podYaml, Closure pippeline) {
     def strPodYaml = libraryResource "$podYaml"
 
-    podTemplate(yaml: strPodYaml) {
+    podTemplate(label: 'maven', containers: [
+            containerTemplate(name: 'maven', image: 'maven:3.3.9-jdk-8-alpine', ttyEnabled: true, command: 'cat'),
+            containerTemplate(name: 'jnlp', image: 'jenkins/jnlp-slave:3.10-1-alpine', args: '${computer.jnlpmac} ${computer.name}', workspaceVolume: 'hostPath  /c/Users/MilindChavan/k8s/app-dependencies')
+    ])  {
         node(POD_LABEL) {
             disableConcurrentBuilds()
             timeout(60) {               
