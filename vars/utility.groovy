4bc272cb30957ef6fc1f48fafbe25d2b59468dcf
milinddevops <46132865+milinddevops@users.noreply.github.com>
2022-04-04 00:40:55 +0530

    Update utility.groovy

--- vars/utility.groovy
+++ vars/utility.groovy
@@ -2,7 +2,7 @@ void runPipeline(String podYaml, Closure pippeline) {
     def strPodYaml = libraryResource "$podYaml"
 
     podTemplate(yaml: strPodYaml) {
-        node(POD_LABEL) {
+        node(label) {
             disableConcurrentBuilds()
             timeout(60) {               
                 pippeline.call()           
